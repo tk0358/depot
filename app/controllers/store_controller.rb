835785b13d0047a55d9c3df516788438316457cb
tk0358 <tk0358a@yahoo.co.jp>
2018-10-05 11:49:46 +0900

    during E1 just changed @line_item at  create action in line_items_controller

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,10 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    if session[:counter].nil?
+      session[:counter] = 0
+    else
+      session[:counter] += 1
+    end
   end
 end
