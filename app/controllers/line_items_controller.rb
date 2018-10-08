6f857ad0bbb3e51e1027781cfa124324df6d2028
tk0358 <tk0358a@yahoo.co.jp>
2018-10-08 11:54:18 +0900

    Task F is finished

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -28,11 +28,11 @@ class LineItemsController < ApplicationController
   def create
     product = Product.find(params[:product_id])
     @line_item = @cart.add_product(product)
-    session[:counter] = 0
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart }
+        format.html { redirect_to store_index_url }
+        format.js { @current_item = @line_item }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,10 +1,7 @@
 class StoreController < ApplicationController
+  include CurrentCart
+  before_action :set_cart
   def index
     @products = Product.order(:title)
-    if session[:counter].nil?
-      session[:counter] = 0
-    else
-      session[:counter] += 1
-    end
   end
 end
